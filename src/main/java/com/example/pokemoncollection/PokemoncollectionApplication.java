f26e9482510867c8ec866fa436e3a8dd792a2c51
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2022-10-21 13:18:31 +0300

    First commit

---
+++ src/main/java/com/example/pokemoncollection/PokemoncollectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemoncollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemoncollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemoncollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokemoncollection/PokemoncollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemoncollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemoncollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
