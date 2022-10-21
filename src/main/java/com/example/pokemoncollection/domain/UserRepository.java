bad82ca7d5ea77be7f533d4f725c88d82c7a928f
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2022-10-21 22:58:57 +0300

    implemented User entity

---
+++ src/main/java/com/example/pokemoncollection/domain/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.pokemoncollection.domain;
+
+public class UserRepository {
+
+}
---
+++ src/main/java/com/example/pokemoncollection/web/UserController.java
@@ -0,0 +1,8 @@
+package com.example.pokemoncollection.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
