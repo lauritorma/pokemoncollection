1a01d974479265c3efe746f8b26fc382860a02f8
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2022-11-09 19:14:44 +0200

    debugging and some css

--- src/main/java/com/example/pokemoncollection/WebSecurityConfig.java
+++ src/main/java/com/example/pokemoncollection/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,8 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		.authorizeRequests()
 		.antMatchers("/signup", "/saveuser").permitAll()
-		.antMatchers("/delete/**").hasRole("ADMIN")
-		.antMatchers("/deleteAll").hasRole("ADMIN")
+//		.antMatchers("/delete/**").hasRole("ADMIN")
+//		.antMatchers("/deleteAll").hasRole("ADMIN")
 		.anyRequest().authenticated()
         .and()
       .formLogin()
@@ -39,6 +40,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
           .permitAll();
 		
          
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web
+	            .ignoring()
+	            .antMatchers("/resources/**", "/static/**", "/css/**","/images/**");
 	}
 	
 	@Autowired
