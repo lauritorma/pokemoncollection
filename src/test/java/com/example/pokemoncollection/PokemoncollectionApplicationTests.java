265a072dd7ad05c63d6fcd8243297c86d8a4f42b
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2022-11-11 19:06:37 +0200

    added tests to type- and versioncontroller

--- src/test/java/com/example/pokemoncollection/PokemoncollectionApplicationTests.java
+++ src/test/java/com/example/pokemoncollection/PokemoncollectionApplicationTests.java
@@ -1,13 +1,51 @@
 package com.example.pokemoncollection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.example.pokemoncollection.web.PokemonController;
+import com.example.pokemoncollection.web.TypeController;
+import com.example.pokemoncollection.web.UserController;
+import com.example.pokemoncollection.web.VersionController;
+
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class PokemoncollectionApplicationTests {
+public class PokemoncollectionApplicationTests {
 
+
+	@Autowired
+	private PokemonController controller;
+	
+	@Autowired 
+	private UserController ucontroller;
+	
+	@Autowired
+	private TypeController tcontroller;
+	
+	@Autowired
+	private VersionController vcontroller;
+	
 	@Test
-	void contextLoads() {
-	}
-
+	 public void contextLoadsPokemonController() throws Exception {
+	 assertThat(controller).isNotNull();
+	 }
+	
+	@Test
+	 public void contextLoadsUserController() throws Exception {
+	 assertThat(ucontroller).isNotNull();
+	 }
+	@Test
+	 public void contextLoadsTypeController() throws Exception {
+	 assertThat(tcontroller).isNotNull();
+	 }
+	@Test
+	 public void contextLoadsVersionController() throws Exception {
+	 assertThat(vcontroller).isNotNull();
+	 }
 }
